70a23fa0b4207af978cb90d5472d3c0a6ca3b419
wualin <1032298871@qq.com>
2018-08-24 09:07:37 +0800

    练习作业

--- python基础/day10/作业.py
+++ python基础/day10/作业.py
@@ -61,33 +61,94 @@
 # print(l1)
 # li[2][2] = 'ALL'
 # print(li)
+#
+# #5.写代码，有如下元组。请按照功能要求实现每一个功能
+# tu = ('alex','eric','rain')
+# #* 计算元组的长度并输出
+# print(len(tu))
+# #* 获取元组的第二个元素，并输出
+# print(tu[2])
+# #*  获取元组第1-2个元素，并输出
+# print(tu[1:2])
+# #* 请使用for，len，rang输出元组的索引
+# for i in range(len(tu)):
+# 	print(i)
+# #* 请使用enumrate输出元组元素和序号
+# for index,t in enumerate(tu):
+# 	print(t,index+10)
+#
+# #6.有如下变量，请实现要求的功能
+# tu = ('alex',[11,22,{'k1':'v1','k2':['age','name'],'k3':(11,22,33)},44])
+# #* 请讲述元组的特性：
+# #答：元组是有序不可变列表
+# #* 请问变量tu中的第一个元素'alex'是否可以被修改
+# # 不可被修改
+# #* 请问tu变量中的'k2'对应的值是什么类型？是否可以被修改？如果可以，请在其后面添加元素'seven
+# # tu[1][2]['k2'].append('seven')
+# # print(tu)			元组中可变元素不可被更改
+# #* tu变量中'k3'对应的值是什么类型...
+# # tu变量中k3对应的值是元组类型，不可被修改
 
-#5.写代码，有如下元组。请按照功能要求实现每一个功能
-tu = ('alex','eric','rain')
-#* 计算元组的长度并输出
-print(len(tu))
-#* 获取元组的第二个元素，并输出
-print(tu[2])
-#*  获取元组第1-2个元素，并输出
-print(tu[1:2])
-#* 请使用for，len，rang输出元组的索引
-for i in range(len(tu)):
-	print(i)
-#* 请使用enumrate输出元组元素和序号
-for index,t in enumerate(tu):
-	print(t,index+10)
+#7.字典
+dic = {'k1':'v1','k2':'v2','k3':[11,22,33]}
+# * 请循环输出所有key
+	# print(k)
+#* 请循环输出所有value
+	# print(dic[k])
+#* 请循环输出所有key和value
+	# print(k,dic[k])
+#*
+#*请在字典中添加一个健值对，'k4':'v4',输出添加后的字典
+# dic['k4'] = 'v4'
+# print(dic)
+#* 修改字典中'k1'对应的值为'alex',输出修改后的字典
+# dic['k1'] = 'alex'
+# print(dic)
+#* 请在k3对应的值中追加一个元素44，输出修改后的字典
+# dic['k3'].append(44)
+# print(dic)
+#* 请在k3对应的值的第一个位置插入元素18，输出修改后的字典
+# dic['k3'].insert(1,18)
+# print(dic)
 
-#6.有如下变量，请实现要求的功能
-tu = ('alex',[11,22,{'k1':'v1','k2':['age','name'],'k3':(11,22,33)},44])
-#* 请讲述元组的特性：
-#答：元组是有序不可变列表
-#* 请问变量tu中的第一个元素'alex'是否可以被修改
-# 不可被修改
-#* 请问tu变量中的'k2'对应的值是什么类型？是否可以被修改？如果可以，请在其后面添加元素'seven
-# tu[1][2]['k2'].append('seven')
-# print(tu)			元组中可变元素不可被更改
-#* tu变量中'k3'对应的值是什么类型...
-# tu变量中k3对应的值是元组类型，不可被修改
+# 转换
+#* 将字符串s = 'alex'转换成列表
+# s = 'alex'
+# s = list(s)
+# print(s)
+#* 将字符串s = 'alex'转换成元组
+# s = 'alex'
+# s = tuple(s)
+# print(s)
+#* 将列表li = 【'Alex'，'seven'】转换成元组
+# li = ['alex','seven']
+# li = tuple(li)
+# print(li)
+#* 将元组tu = ['alex','seven']转换成列表
+# tu = ['alex','seven']
+# tu = list(tu)
+# print(tu)
+#* 将列表li = ['alex','seven']转换成字典，且字典的key按照10开始往后递增
+# li = ['alex','seven']
+# dic = {}
+# for index,i in enumerate(li):
+# 	dic[index+10] = i
+# print(dic)
 
-
+#9.元素分类
+#有如下集合[11,22,33,44,55,66,77,88,99,90],将所有大于66的值保存至字典的第一个key中，将小于66的值保存至第二个key的值中
+# 即：{'k1':大于66的所有值，'k2':小于66的所有值}
+# li = [11,22,33,44,55,66,77,88,99,90]
+# l1 = []
+# l2 = []
+# dic = {}
+# for i in li:
+# 	if i > 66:
+# 		l1.append(i)
+# 	if i < 66:
+# 		l2.append(i)
+# dic['k1'] = l1
+#
+# dic['k2'] = l2
+# print(dic)
 
---
+++ python基础/day11/作业.py
@@ -0,0 +1,40 @@
+# li = ['手机','电脑','鼠标垫','游艇']
+# run = True
+# print('-----商品列表-----')
+# for index, i in enumerate(li):
+# 	print(index, i)
+# while run:
+#
+# 	num = input('请输入需要添加的物品或者需要显示商品的序号')
+# 	if num.isdigit():
+# 		print(li[int(num)])
+# 	if num == 'l':
+# 		print(li)
+# 	elif num.isalpha():
+# 		li.append(num)
+# 		print("添加成功")
+#
+
+##交集
+# l1 = {'tom','jack','jom','tonny'}
+# l2 = {'tom','jom'}
+# print(l1.intersection(l2))
+# print(l1&l2)
+
+# 合集
+# l1 = {'tom','jack','jom','tonny'}
+# l2 = {'tom','jom','lisa','namei'}
+# print(l1.union(l2))
+# print(l1|l2)
+
+#差集
+# l1 = {'tom','jack','jom','tonny'}
+# l2 = {'tom','jom','lisa','namei'}
+# print(l1.difference(l2))
+# print(l1-l2)
+
+# 对称差集
+l1 = {'tom','jack','jom','tonny'}
+l2 = {'tom','jom','lisa','namei'}
+print(l1.symmetric_difference(l2))
+print(l1^l2)
