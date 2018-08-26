4f0a2a5fceb407078b6823532509c03c9901ee90
wualin <1032298871@qq.com>
2018-08-26 11:43:31 +0800

    函数

---
+++ 文件、函数与常用模块/day2-函数/函数的基本语法.py
@@ -0,0 +1,20 @@
+# 函数的基本语法
+# def sayhi():
+# 	print("hello world")
+# 	print("my name is xixixi...")
+#
+# def cale(x,y):
+# 	res = x**y
+# 	print(res)
+#
+#
+# sayhi()
+# cale(2,10)
+
+
+## 默认参数
+## def infomation(name, age, country="中国"):
+## 	print(name, age, country)
+##
+## infomation("吴阿麟","22")
+## infomation("jack",'12','USA')
