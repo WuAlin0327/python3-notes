90f6db6a6f043c381e4b391ed1543deaa9c43901
wualin <1032298871@qq.com>
2018-09-08 22:04:12 +0800

    员工信息增删改查作业完成～～～！

--- 文件、函数与常用模块/day7/员工信息增删改查.py
+++ 文件、函数与常用模块/day7/员工信息增删改查.py
@@ -2,11 +2,11 @@ import tabulate
 import os
 
 FILENAME = 'staff_table.txt'
-DATA = ['id','name','age','phone','career','date']
+DATA = ['id','name','age','phone','dept','date']
 FUNC = ['find','add','updata','del']
 def staff_data():
 	'''
-	数据结构
+	打开文件将文件内容输出为以字典形式的数据结构
 	:return:
 	'''
 	f = open(FILENAME,'r')
@@ -16,7 +16,7 @@ def staff_data():
 		'name':[],
 		'age':[],
 		'phone':[],
-		'career':[],
+		'dept':[],
 		'date':[]
 	}
 	file = f.readlines()
@@ -25,7 +25,7 @@ def staff_data():
 		staff['name'].append(i.strip().split(',')[1])
 		staff['age'].append(i.strip().split(',')[2])
 		staff['phone'].append(i.strip().split(',')[3])
-		staff['career'].append(i.strip().split(',')[4])
+		staff['dept'].append(i.strip().split(',')[4])
 		staff['date'].append(i.strip().split(',')[5])
 	f.close()
 	return staff
@@ -47,11 +47,25 @@ def save_data(data):
 		f.write(','.join(i)+'\n')
 	f.close()
 	os.rename('%s.new'%(FILENAME),FILENAME)
-def del_data():
+def syntax_del(cmd):
 	'''
 	删除数据
 	:return:
 	'''
+	snap = []
+	c_del = cmd.split()[-1].strip()
+	for index,i in enumerate(STAFF_DATA['id']):
+		snap_1 = []
+		if i == c_del:
+			l = index
+			for s in DATA:
+				snap_1.append(STAFF_DATA[s][l])
+				del STAFF_DATA[s][l]
+			snap.append(snap_1)
+			save_data(STAFF_DATA)
+			print("删除了1条数据"+'\n',tabulate.tabulate(snap,headers=(i for i in DATA)))
+
+
 
 def big(decide,condition,res_data):# 大于
 	'''
@@ -64,7 +78,7 @@ def big(decide,condition,res_data):# 大于
 
 
 	if res_data != '*':
-		snap = []
+		snap = [] #临时变量
 		for i in STAFF_DATA[decide]:
 			if int(i) > int(condition):
 				index_big = STAFF_DATA[decide].index(i)
@@ -72,19 +86,22 @@ def big(decide,condition,res_data):# 大于
 				for k in res_data:
 					snap_1.append(STAFF_DATA[k][index_big])
 				snap.append(snap_1)
-		print(tabulate.tabulate(snap,headers=(i for i in res_data)))
 		print("查询到了%s 条记录" % (len(snap)))
+		print(tabulate.tabulate(snap,headers=(i for i in res_data)))
+
 	else:
-		p_all = []
+		snap = []
 		for i in STAFF_DATA[decide]:
 			if int(i) > int(condition):
 				index_big = STAFF_DATA[decide].index(i)
-				snap = []
+				snap_1 = []
 				for k in DATA:
 					snap.append(STAFF_DATA[k][index_big])
-				p_all.append(snap)
-		print(tabulate.tabulate(p_all,headers=(i for i in DATA)))
-		print("查询到了%s 条记录" % (len(p_all)))
+				snap.append(snap_1)
+
+			print("查询到了%s 条记录" % (len(snap)))
+			print(tabulate.tabulate(snap,headers=(i for i in DATA)))
+
 
 def small(decide,condition,res_data):# 小于
 
@@ -98,33 +115,34 @@ def small(decide,condition,res_data):# 小于
 				for k in res_data:
 					snap_1.append(STAFF_DATA[k][index_big])
 				snap.append(snap_1)
-		print(tabulate.tabulate(snap,headers=(i for i in res_data)))
 		print("查询到了%s 条记录" % (len(snap)))
+		print(tabulate.tabulate(snap,headers=(i for i in res_data)))
 	else:
-		p_all = []
+		snap = []
 		for i in STAFF_DATA[decide]:
 			if int(i) < int(condition):
 				index_big = STAFF_DATA[decide].index(i)
-				snap = []
+				snap_1 = []
 				for k in DATA:
 					snap.append(STAFF_DATA[k][index_big])
-				p_all.append(snap)
-		print(tabulate.tabulate(p_all,headers=(i for i in DATA)))
-		print("查询到了%s 条记录" % (len(p_all)))
+				snap.append(snap_1)
+		print("查询到了%s 条记录" % (len(snap)))
+		print(tabulate.tabulate(snap,headers=(i for i in DATA)))
+
 def equal(decide,condition,res_data):# 等于
 	snap = []
-
-	for i in STAFF_DATA[decide]:
-
+	for index,i in enumerate(STAFF_DATA[decide]):
 		if i == condition:
-			index_big = STAFF_DATA[decide].index(i)
+			index_equal = index
 			snap_1 = []
-			for k in DATA:
-				snap_1.append(STAFF_DATA[k][index_big])
-			snap.append(snap_1)
+			if i == condition:
+
+				for k in DATA:
+					snap_1.append(STAFF_DATA[k][index_equal])
+				snap.append(snap_1)
 	print(tabulate.tabulate(snap, headers=(i for i in DATA)))
 	print("查询到了%s 条记录"%(len(snap)))
-def like(decide,condition):# 相似
+def like(decide,condition,res_data):# 相似
 	snap = []
 
 	for i in STAFF_DATA[decide]:
@@ -137,15 +155,7 @@ def like(decide,condition):# 相似
 	print(tabulate.tabulate(snap,headers=(i for i in DATA)))
 	print("查询到了%s 条记录"%(len(snap)))
 
-def print_all():# 打印所有
-	p_all = []
-	for index,l in enumerate(STAFF_DATA['id']):
-		snap = []
-		for i in DATA:
-			snap.append(STAFF_DATA[i][index])
-		print(snap)
-
-def syntax_add(cmd):
+def syntax_add(cmd): # 增加
 	add_info = cmd.strip().split()[2:]
 
 	if add_info[2] not in STAFF_DATA['phone']:
@@ -154,7 +164,7 @@ def syntax_add(cmd):
 		STAFF_DATA['name'].append(add_info[0])
 		STAFF_DATA['age'].append(add_info[1])
 		STAFF_DATA['phone'].append(add_info[2])
-		STAFF_DATA['career'].append(add_info[3])
+		STAFF_DATA['dept'].append(add_info[3])
 		STAFF_DATA['date'].append(add_info[4])
 		save_data(STAFF_DATA)
 		print("增加了1条数据")
@@ -163,7 +173,7 @@ def syntax_add(cmd):
 
 
 
-def syntax_find(cmd):
+def syntax_find(cmd): # 查找
 	'''
 	传入输入的语句，解析语句
 	:param cmd:
@@ -191,18 +201,44 @@ def syntax_find(cmd):
 				for k in calculation:
 					if where_right[1] == k:
 						calculation[k](where_right[0],where_right[2],res,)# 执行对应函数
-		else:
-			print("%s 不在%s"%(where_right.split[0],(i for i in DATA)))
 	else:
 		print("语法错误")
+
+def syntax_update(cmd):#修改数据
+	'''
+	解析输入更新语句，获取需要修改的值，然后用新值替换后写入文件
+	:param cmd:
+	:return:
+	'''
+	res1_cmd,res2_cmd = cmd.strip().split('where')
+	old_res = res2_cmd.split('=')
+	new_res = res1_cmd.split('set')[-1].split('=')
+	snap = []
+	for index,i in enumerate(STAFF_DATA[old_res[0].strip()]):
+		if i == old_res[-1].strip():
+			snap.append(index)
+			index = index
+			STAFF_DATA[new_res[0].strip()][index] = new_res[-1].strip()
+	print("修改了%s条记录！"%(len(snap)))
+	save_data(STAFF_DATA)
 
 
 def main():
 	while True:
+		STAFF_DATA = staff_data()
 		cmd = input('>>>')
-		if cmd.strip().split()[0]== 'find':
-			syntax_find(cmd.strip())
+		if 'where' in cmd.strip().split():
+			if cmd.strip().split()[0]=='find':
+				syntax_find(cmd)
+			if cmd.strip().split()[0]=='update':
+				syntax_update(cmd)
+
 		if cmd.strip().split()[0]=='add':
 			syntax_add(cmd)
+		if cmd.strip().split()[0]=='del':
+			syntax_del(cmd)
+		else:
+			print("输入错误！语句不是以'find','del','add','update'开头")
+
 
 main()
