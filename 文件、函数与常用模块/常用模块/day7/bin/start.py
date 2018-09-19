75333a5d25846512a53940c3b46d887e14481e08
wualin <1032298871@qq.com>
2018-09-19 23:08:09 +0800

    作业

--- 文件、函数与常用模块/常用模块/day7/bin/start.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath('luffy.json')))
-
-f = open('%s/account/luffy.json'%(BASE_DIR))
-date = json.load(f)
-print(date)
