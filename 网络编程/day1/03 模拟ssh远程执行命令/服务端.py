417598bc0f162120acffe24b90ebaa030d32a566
wualin <1032298871@qq.com>
2018-10-08 18:27:11 +0800

    网络编程

--- 网络编程/day1/03 模拟ssh远程执行命令/服务端.py
+++ 网络编程/day1/03 模拟ssh远程执行命令/服务端.py
@@ -1,5 +1,5 @@
 import socket
-import os
+import subprocess
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
@@ -14,12 +14,15 @@ while True:#链接循环
 			#接收命令
 			cmd = conn.recv(1024)
 			if not cmd:break # 当客户端结束运行时，跳出循环，liunx操作系统
-			print('客户端的数据:',cmd)
-
+			obj = subprocess.Popen(cmd.decode('utf-8'),shell=True,
+								   stdout=subprocess.PIPE,
+								   stderr=subprocess.PIPE)
+			stdout = obj.stdout.read()
+			stderr = obj.stderr.read()
 			#执行命令
 
 			#把命令返回
-			conn.send(cmd.upper())
+			conn.send(stdout+stderr)
 		except ConnectionResetError:#使用于当客户端软件运行结束时，监测到客户端软件结束跳出循环
 			break
 
