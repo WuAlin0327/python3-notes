a2c4f596a12361b0dd31ae2d4a5cd370ae01a6fe
wualin <1032298871@qq.com>
2018-11-15 22:18:53 +0800

    复习

---
+++ leecode.py
@@ -0,0 +1,68 @@
+# import asyncio
+# import random
+#
+# async def task(n):
+# 	wait = random.randint(1,5)
+# 	print('start run',wait,n)
+# 	await asyncio.sleep(wait)
+# 	print('end run',wait,n)
+#
+#
+# async def main():
+# 	await asyncio.gather(*[task(n) for n in range(10)])
+#
+# asyncio.run(main())
+
+# import requests
+# import asyncio
+# import time
+#
+# async def get_url(url):
+# 	print('开始抓取')
+# 	res = requests.get(url)
+# 	if res.status_code == 200:
+# 		return {
+# 			'url':url,
+# 			'text':len(res.text)
+# 		}
+#
+# async def main():
+# 	url_list = [
+# 		'https://cloud.tencent.com/developer/article/1194415',
+# 		'https://www.cnblogs.com/wualin/',
+# 		'https://www.luffycity.com/study',
+# 		'https://github.com/WuAlin0327'
+# 	]
+# start = time.time()
+# asyncio.run(main())
+# end = time.time()
+# print(end-start)
+# from gevent import monkey;monkey.patch_all()
+# import requests
+# import time
+# import gevent
+# import random
+# def get_url(url):
+# 	print('开始抓取')
+# 	res = requests.get(url)
+# 	if res.status_code == 200:
+# 		return url,res.text
+#
+# if __name__ == '__main__':
+# 	start = time.time()
+# 	url_list = [
+# 		'https://cloud.tencent.com/developer/article/1194415',
+# 		'https://www.cnblogs.com/wualin/',
+# 		'https://www.luffycity.com/study',
+# 		'https://github.com/WuAlin0327'
+# 	]
+# 	g_lis = []
+# 	for url in url_list:
+# 		g = gevent.spawn(get_url,url)
+# 		g_lis.append(g)
+#
+# 	for g in g_lis:
+# 		url,text = g.get()
+# 		print(url,len(text))
+# 	end = time.time()
+# 	print(end-start)
--- 并发编程/多线程/Event.py
+++ 并发编程/多线程/Event.py
@@ -1,23 +1,52 @@
-from threading import Thread,Event
+# from threading import Thread,Event
+# import time
+# event = Event()
+# def student(name):
+# 	print('学生 %s正在听课'%name)
+# 	event.wait()
+# 	print('学生%s下课了'%name)
+#
+# def teacher(name):
+# 	print('老师%s 正在上课'%name)
+# 	time.sleep(7)
+# 	print('老师%s 让学生下课了'%name)
+# 	event.set()
+#
+# if __name__ == '__main__':
+# 	s1 = Thread(target=student,args=('wualin',))
+# 	s2 = Thread(target=student,args=('wxx',))
+# 	s3 = Thread(target=student,args=('zxx',))
+# 	t1 = Thread(target=teacher,args=('egon',))
+# 	s1.start()
+# 	s2.start()
+# 	s3.start()
+# 	t1.start()
+from threading import Event,Thread
+import random
 import time
-event = Event()
-def student(name):
-	print('学生 %s正在听课'%name)
-	event.wait()
-	print('学生%s下课了'%name)
 
-def teacher(name):
-	print('老师%s 正在上课'%name)
-	time.sleep(7)
-	print('老师%s 让学生下课了'%name)
-	event.set()
+def cheak(e):
+	wait = random.randint(1,3)
+	print(wait)
+	if wait <= 2:
+		print('网络连接成功')
+		e.set()
+	else:
+		e.set()
+		e.clear()
 
-if __name__ == '__main__':
-	s1 = Thread(target=student,args=('wualin',))
-	s2 = Thread(target=student,args=('wxx',))
-	s3 = Thread(target=student,args=('zxx',))
-	t1 = Thread(target=teacher,args=('egon',))
-	s1.start()
-	s2.start()
-	s3.start()
-	t1.start()
+
+def run_sql(e):
+	print('正在尝试连接数据库')
+	e.wait()
+	if e.is_set():
+		time.sleep(3)
+		print('数据库连接成功')
+	else:
+		print('连接超时')
+
+e = Event()
+t1 = Thread(target=cheak,args=(e,))
+t2 = Thread(target=run_sql,args=(e,))
+t1.start()
+t2.start()
