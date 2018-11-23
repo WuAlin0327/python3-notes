2a9791bcdbf9c2ac0577216dcdd1f68df68f9f52
wualin <1032298871@qq.com>
2018-11-23 12:39:10 +0800

    前端

---
+++ 前端/JavaScript/js/index.js
@@ -0,0 +1,11 @@
+// 这个是注释
+var score = 'good';
+switch (score) {
+    case 'good':
+        console.log('good');
+        break;
+    case 'better':
+        console.log('brtter');
+        break;
+    }
+    alert('结束了');
